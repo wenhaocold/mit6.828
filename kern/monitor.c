494b12effc8c4665068e395762e42bde09f4fffb
wenhaocold <wenhaocold@163.com>
2022-05-22 20:06:51 +0800

    acomplish lab1

--- kern/monitor.c
+++ kern/monitor.c
@@ -57,7 +57,49 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	// // Your code here.
+    // uint32_t ebp = read_ebp(); 
+    // uint32_t* p_old_ebp; 
+    // uint32_t* p_ret; 
+    // // struct Eipdebuginfo info;
+    // while (ebp) {
+    //     p_old_ebp = (uint32_t*)ebp;
+    //     p_ret = p_old_ebp + 1;
+    //     cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", 
+    //             ebp, *p_ret, *(p_old_ebp+2), 
+    //             *(p_old_ebp+3), *(p_old_ebp+4), 
+    //             *(p_old_ebp+5), *(p_old_ebp+6));
+    //
+    //     // debuginfo_eip(*p_ret, &info);
+    //     // cprintf("%s ", info.eip_file);
+    //     // cprintf("%s ", info.eip_fn_name);
+    //     // cprintf("%d\n", info.eip_line);
+    //     ebp = *p_old_ebp;
+    // }
+    uint32_t ebp = read_ebp();
+    uint32_t eip = 0;
+    struct Eipdebuginfo info;
+    #define TO_INT(x)  *((uint32_t*)(x))
+    while (ebp) {
+        eip = TO_INT((ebp+4));
+        cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",
+            ebp,                /*ebp*/
+            eip,                /*eip*/
+            TO_INT((ebp+8)),    /*arg1*/
+            TO_INT((ebp+12)),   /*arg2*/
+            TO_INT((ebp+16)),   /*arg3*/
+            TO_INT((ebp+20)),   /*arg4*/
+            TO_INT((ebp+24)));  /*arg5*/
+
+        if(!debuginfo_eip(eip, &info)) {
+        cprintf("%s:%d: %.*s+%d\n",
+              info.eip_file,
+              info.eip_line,
+              info.eip_fn_namelen, info.eip_fn_name,
+              eip - info.eip_fn_addr);
+        }
+        ebp = TO_INT(ebp);
+    }
 	return 0;
 }
 
